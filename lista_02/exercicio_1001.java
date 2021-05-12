961b6bfa7cfbab1baad21e6079349a75f1eab666
lukete <lsschmalz@gmail.com>
2021-05-12 20:14:47 -0300

    Adicionado exercicio_1001.java

---
+++ lista_02/exercicio_1001.java
@@ -0,0 +1,13 @@
+package aulaheredia;
+
+
+import java.util.Scanner;
+public class exercicio_1001 {
+    public static void main(String[] args){
+        Scanner leitura = new Scanner(System.in);
+        int A = leitura.nextInt();
+        int B = leitura.nextInt();
+        int X = A + B;
+        System.out.println("X = "+X);
+    }    
+}
