abcd2188b6e401c4e4e27476b73bb60a5487fb0d
lukete <lsschmalz@gmail.com>
2021-05-12 22:06:50 -0300

    Adicionado exercicio_1007.java

---
+++ lista_02/exercicio_1007.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+ 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+import java.util.Scanner; 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner leitura = new Scanner(System.in);
+        int A,B,C,D,DIFERENCA;
+        A = leitura.nextInt();
+        B = leitura.nextInt();
+        C = leitura.nextInt();
+        D = leitura.nextInt();
+        DIFERENCA = (A*B-C*D);
+        System.out.println("DIFERENCA = "+DIFERENCA);
+ 
+    }
+ 
+}
