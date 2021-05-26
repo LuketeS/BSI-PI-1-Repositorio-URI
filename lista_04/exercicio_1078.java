3b48ee37d8683ade1fe382d507d93af2b69bd30f
lukete <lsschmalz@gmail.com>
2021-05-26 20:38:43 -0300

    Adicionado exercicio_1078.java

---
+++ lista_04/exercicio_1078.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.util.Scanner;
+ 
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner leitura = new Scanner(System.in);
+        int N = leitura.nextInt();
+        System.out.println("1 x "+ N +" = " + 1*N);
+        System.out.println("2 x "+ N +" = " + 2*N);
+        System.out.println("3 x "+ N +" = " + 3*N);
+        System.out.println("4 x "+ N +" = " + 4*N);
+        System.out.println("5 x "+ N +" = " + 5*N);
+        System.out.println("6 x "+ N +" = " + 6*N);
+        System.out.println("7 x "+ N +" = " + 7*N);
+        System.out.println("8 x "+ N +" = " + 8*N);
+        System.out.println("9 x "+ N +" = " + 9*N);
+        System.out.println("10 x "+ N +" = " + 10*N);
+        
+        
+        
+    }
+ 
+}
